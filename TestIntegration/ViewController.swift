90de20586d7639421d0ab4997c28761a11266b5f
Denis Panov <v-depano@microsoft.com>
2018-07-17 13:24:23 +0300

    Initial commit app

---
+++ TestIntegration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestIntegration
+//
+//  Created by Administrator on 7/17/18.
+//  Copyright © 2018 Administrator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
